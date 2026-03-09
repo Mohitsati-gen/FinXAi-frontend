163db587932e1ccbd54698b4e095932fb661fcf9
mohit <abhisheksati987@gmail.com>
2026-03-09 23:44:24 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+      <main className='bg-amber-300 min-h-screen'>hello this is gonna be my new project</main>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
